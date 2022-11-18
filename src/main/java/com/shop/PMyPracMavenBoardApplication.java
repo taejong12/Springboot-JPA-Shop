a65940622da0dde8bb81cd1aec3d58e0a667ba0d
taejong12 <117630566+taejong12@users.noreply.github.com>
2022-11-18 17:59:10 +0900

    환경세팅 완료

---
+++ src/main/java/com/shop/PMyPracMavenBoardApplication.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PMyPracMavenBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PMyPracMavenBoardApplication.class, args);
+	}
+
+}
